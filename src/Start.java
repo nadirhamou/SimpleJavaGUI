c2fcc1412ba8cd4153ea00c6b04da4e8e1bf24e3
nadirhamou <mohamed.hamou@ensia.edu.dz>
2023-03-07 11:03:18 +0100

    This is a commit message

--- src/Start.java
+++ src/Start.java
@@ -4,6 +4,8 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		//this class displays Hello world message
+		//this is a comment
 
 	}
 
